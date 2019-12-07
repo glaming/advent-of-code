d3d2df0a72b7d8c0740aabc668484fb4654bd233
Graham Laming <graham@laming.me>
2019-12-07 15:15:55 +0000

    Day 07 - Part 1

--- day07/main.go
+++ day07/main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/glaming/advent-of-code-2019/intcode"
+	"log"
 	"strconv"
 	"strings"
 )
@@ -32,4 +33,68 @@ func runAmplifierSequence(tape intcode.Tape, phaseSettings []int) (int, error) {
 	}
 
 	return inputSignal, nil
+}
+
+func runeToInt(r rune) int {
+	if r >= '0' && r <= '9' {
+		return int(r - '0')
+	}
+	return -1
+}
+
+// Looking to find the settings for maximal signal output
+func findPhaseSettings(tape intcode.Tape) ([]int, int, error) {
+	// Find valid phases to test
+	var phasesToTest [][]int
+loop:
+	for i := 0; i <= 55555; i++ {
+		var ps []int
+		phaseStr := fmt.Sprintf("%05d", i)
+		for _, p := range phaseStr {
+			if p < '0' || p > '4' {
+				continue loop
+			}
+
+			for _, p2 := range ps {
+				if p2 == runeToInt(p) {
+					continue loop
+				}
+			}
+			ps = append(ps, runeToInt(p))
+		}
+
+		phasesToTest = append(phasesToTest, ps)
+	}
+
+	maxSignal := -1
+	maxPhaseSetting := []int{0}
+
+	for _, ps := range phasesToTest {
+		signal, err := runAmplifierSequence(tape, ps)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		if signal > maxSignal {
+			maxSignal = signal
+			maxPhaseSetting = ps
+		}
+	}
+
+	return maxPhaseSetting, maxSignal, nil
+}
+
+func main() {
+	tape, err := intcode.ReadTape("day07/input.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ps, signal, err := findPhaseSettings(tape)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Phase settings: %v\nMax signal: %d", ps, signal)
+
 }
--- day07/main_test.go
+++ day07/main_test.go
@@ -2,31 +2,32 @@ package main
 
 import (
 	"github.com/glaming/advent-of-code-2019/intcode"
+	"reflect"
 	"testing"
 )
 
-func TestRunAmplifierSequence(t *testing.T) {
-	tt := []struct {
-		tape           intcode.Tape
-		phaseSettings  []int
-		expectedSignal int
-	}{
-		{
-			intcode.Tape{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0},
-			[]int{4, 3, 2, 1, 0},
-			43210,
-		}, {
-			intcode.Tape{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0},
-			[]int{0, 1, 2, 3, 4},
-			54321,
-		}, {
-			intcode.Tape{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0},
-			[]int{1, 0, 4, 3, 2},
-			65210,
-		},
-	}
+var testTable = []struct {
+	tape           intcode.Tape
+	phaseSettings  []int
+	expectedSignal int
+}{
+	{
+		intcode.Tape{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0},
+		[]int{4, 3, 2, 1, 0},
+		43210,
+	}, {
+		intcode.Tape{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0},
+		[]int{0, 1, 2, 3, 4},
+		54321,
+	}, {
+		intcode.Tape{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0},
+		[]int{1, 0, 4, 3, 2},
+		65210,
+	},
+}
 
-	for i, test := range tt {
+func TestRunAmplifierSequence(t *testing.T) {
+	for i, test := range testTable {
 		signal, err := runAmplifierSequence(test.tape, test.phaseSettings)
 		if err != nil {
 			t.Errorf("test %d, encountered error %s", i+1, err)
@@ -34,6 +35,19 @@ func TestRunAmplifierSequence(t *testing.T) {
 
 		if signal != test.expectedSignal {
 			t.Errorf("expected signal %d, got %d", test.expectedSignal, signal)
+		}
+	}
+}
+
+func TestFindPhaseSettings(t *testing.T) {
+	for i, test := range testTable {
+		ps, err := findPhaseSettings(test.tape)
+		if err != nil {
+			t.Errorf("test %d, encountered error %s", i+1, err)
+		}
+
+		if !reflect.DeepEqual(ps, test.phaseSettings) {
+			t.Errorf("expected phase settings %v, got %v", test.phaseSettings, ps)
 		}
 	}
 }
