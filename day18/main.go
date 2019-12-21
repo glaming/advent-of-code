96af801c25b6db9d22f03c19c3f8ef4a0d4c9c99
Graham Laming <graham@laming.me>
2019-12-21 15:35:39 +0000

    Day 18 - Seems to be valid solution, but can't complete for input_test_4

---
+++ day18/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+type (
+	point struct {
+		x, y int
+	}
+
+	state struct {
+		point
+		visited map[point]bool
+		keys    map[rune]bool
+		steps   int
+	}
+)
+
+func readMap(filename string) ([][]rune, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var m [][]rune
+
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		line := s.Text()
+		m = append(m, []rune(line))
+	}
+
+	return m, s.Err()
+}
+
+func isDoor(r rune) bool {
+	if r >= 'A' && r <= 'Z' {
+		return true
+	}
+	return false
+}
+
+func isKey(r rune) bool {
+	if r >= 'a' && r <= 'z' {
+		return true
+	}
+	return false
+}
+
+func newState(prev state, p point) state {
+	s := state{p, make(map[point]bool), make(map[rune]bool), prev.steps + 1}
+	for k := range prev.visited {
+		s.visited[k] = true
+	}
+	for k := range prev.keys {
+		s.keys[k] = true
+	}
+	return s
+}
+
+func findShortestPath(m [][]rune) int {
+	// Find start + how many keys
+	start, totalKeys := point{}, 0
+	for y, line := range m {
+		for x, r := range line {
+			if r == '@' {
+				start = point{x, y}
+			}
+			if isKey(r) {
+				totalKeys++
+			}
+		}
+	}
+
+	queue := []state{{start, make(map[point]bool), make(map[rune]bool), 0}}
+
+	for len(queue) > 0 {
+		var head state
+		head, queue = queue[0], queue[1:]
+
+		if len(head.keys) == totalKeys {
+			return head.steps
+		}
+
+		head.visited[head.point] = true
+
+		adjacent := []point{
+			{head.x - 1, head.y},
+			{head.x + 1, head.y},
+			{head.x, head.y - 1},
+			{head.x, head.y + 1},
+		}
+
+		for _, a := range adjacent {
+			r := m[a.y][a.x]
+			if head.visited[a] {
+				continue
+			}
+			if r == '#' {
+				continue
+			}
+			if _, ok := head.keys[r+('a' - 'A')]; isDoor(r) && !ok {
+				continue
+			}
+
+			s := newState(head, a)
+
+			// If found new key, add key and clear path
+			if _, ok := head.keys[r]; isKey(r) && !ok {
+				s.keys[r] = true
+				s.visited = make(map[point]bool)
+			}
+
+			queue = append(queue, s)
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	m, err := readMap("day18/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sp := findShortestPath(m)
+	log.Printf("Shortest path: %d\n", sp)
+}
---
+++ day18/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func TestFindShortestPath(t *testing.T) {
+	tt := []struct{
+		filename string
+		expected int
+	}{
+		{"input_test_1.txt", 8},
+		{"input_test_2.txt", 86},
+		{"input_test_3.txt", 132},
+		//{"input_test_4.txt", 136},
+		{"input_test_5.txt", 81},
+	}
+
+	for _, test := range tt {
+		m, err := readMap(test.filename)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		sp := findShortestPath(m)
+		if sp != test.expected {
+			t.Errorf("For %s, expected %d, got %d", test.filename, test.expected, sp)
+		}
+	}
+}
