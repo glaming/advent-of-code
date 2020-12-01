c0911f9ab4ffe0fc072c73a126d2005163fb17cb
Graham Laming <graham@laming.me>
2020-12-01 14:01:44 +0000

    Day 1 - Part 1

---
+++ 2020/day01/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func readIntList(filename string) ([]int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var nums []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+
+		nums = append(nums, num)
+	}
+
+	return nums, nil
+}
+
+func main() {
+	nums, err := readIntList("2020/day01/input.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	targetNumber := 2020
+	for _, m := range nums {
+		for _, n := range nums {
+			if m + n == targetNumber {
+				fmt.Sprintf("Solution: %d", m * n)
+				os.Exit(0)
+			}
+		}
+	}
+}
