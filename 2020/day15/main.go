16f73bb069767309b048db9961789001805315c5
Graham Laming <graham@laming.me>
2020-12-15 09:57:24 +0000

    Day 15 - Part 1

---
+++ 2020/day15/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{1,0,15,2,10,13}
+
+	lastSpoken := make(map[int]int, 0)
+	for i, n := range numbers {
+		lastSpoken[n] = i+1
+	}
+
+	for i := len(numbers); i < 2020; i++ {
+		prevNum := numbers[i-1]
+		n, ok := lastSpoken[prevNum]
+
+		spokenNum := 0
+		if ok {
+			spokenNum = i - n
+		}
+		lastSpoken[prevNum] = i
+		numbers = append(numbers, spokenNum)
+	}
+
+	fmt.Println(numbers[2019])
+}
